f5833c361922201944b17c9f5cc1d3676407d133
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-07-03 10:09:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php
+<?php 
+session_start();
+
+?>
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -18,16 +21,29 @@
                 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
               </svg>
             </div>
-            <input id="search" type="text" placeholder="What are you looking for?" />
+           <form method="get" action="result.php" enctype="multipart/form-data">
+            <input id="search" type="text" name="search" placeholder="What are you looking for?" />
           </div>
           <div class="input-field second-wrap">
-            <button class="btn-search" type="button">SEARCH</button>
+            <input type="submit" class="btn-search" onclick="func()">
+            
+          </div>
+        </form>
           </div>
         </div>
         <span class="info">ex. Mobiles,Accessories,Gadgets</span>
       </form>
     </div>
+    <script type="text/javascript">
+      function func(){
+        var search=document.getElementById("search").value;
+        // alert(search);
+        var url ="result.php?search=" +search;
+        window.open(url);
+      }
+    </script>
     <script src="js/extention/choices.js"></script>
-  </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+    
+
+  </body>
 </html>
-?>
